5db547ce92a875e13d6acfc5421467b9389eb283
jwdeitch <jwdeitch@gmail.com>
2018-03-04 15:29:08 -0500

    hashids implementation

--- src/extension.c
+++ src/extension.c
@@ -1,4 +1,19 @@
-#include "extension.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "postgres.h"
+#include "fmgr.h"
+#include "utils/builtins.h"
+#include "guc.h"
+#include "hashids.h"
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+extern void _PG_init(void);
+
 
 void
 _PG_init(void) {
@@ -7,30 +22,34 @@ _PG_init(void) {
 	_guc_init();
 }
 
-char *
-generate_token(size_t desired_str_length, int collision_sensitivity) {
+const char *alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
 
-	if (likelihood_of_collision(12, 12)) {
-		char *test_key = palloc0(sizeof(char) * desired_str_length);
-		gen_rand_string(test_key, desired_str_length);
-		printf("trying -- %s \r\n", test_key);
-	}
-}
 
-static char *
-gen_rand_string(char *str, size_t size) {
-	if (size) {
-		--size;
-		for (size_t n = 0; n < size; n++) {
-			long key = random() % (int) (sizeof charset - 1);
-			str[n] = charset[key];
-		}
-		str[size] = '\0';
-	}
-	return str;
-}
+PG_FUNCTION_INFO_V1( id_encode );
 
-double
-likelihood_of_collision(size_t key_length_count, int record_count) {
-	return (record_count / pow(sizeof(charset), key_length_count)) * 100;
+Datum
+id_encode(PG_FUNCTION_ARGS) {
+	unsigned long long number;
+	text *hash_string;
+	hashids_t *hashids;
+
+	unsigned int bytes_encoded;
+	char *hash;
+
+	number = PG_GETARG_INT64(0);
+
+	hashids = hashids_init3(NULL, 5, alphabet);
+
+	hash = calloc(hashids_estimate_encoded_size(hashids, 1, &number), 1);
+
+	bytes_encoded = hashids_encode_one(hashids, hash, number);
+	hash_string = (text *) palloc(bytes_encoded);
+
+	SET_VARSIZE(hash_string, bytes_encoded + VARHDRSZ);
+	strncpy(VARDATA(hash_string), hash, bytes_encoded);
+
+	hashids_free(hashids);
+	PG_RETURN_TEXT_P(hash_string);
+
+	free(hash);
 }
